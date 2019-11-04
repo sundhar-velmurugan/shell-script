2d557dcdb1e3969bf7fbd77180849078acd9f860
Sundhar V <sundhar811@gmail.com>
2019-11-04 19:01:15 +0530

    Update npm_i.sh

--- Automate installation of Node modules/npm_i.sh
+++ Automate installation of Node modules/npm_i.sh
@@ -6,6 +6,7 @@ dir_path='directory'
 for DIR in $dir_path*
 do
   if [ -d ${DIR} ]; then
+    echo "$(tput setaf 2)CURRENT DIRECTORY: $(tput setab 5)${DIR}$(tput sgr 0)"
     cd $DIR
     rm -rf package-lock.json node_modules
     npm i
